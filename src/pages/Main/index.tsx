38ca018516cdc014161bf56ea16e4f6b7dcbe803
leohenrique8 <theexpendablesgames@gmail.com>
2024-02-14 21:31:02 -0300

    Primeiro commit

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,15 +1,73 @@
+import { FormEvent, useRef, useState } from 'react'
 import { Container } from './styles'
 
 export default function Main() {
+  const valueInputRef = useRef<HTMLInputElement>(null);
+  const fromConvertInputRef = useRef<HTMLSelectElement>(null);
+  const toConvertInputRef = useRef<HTMLSelectElement>(null);
+  const [show, setShow] = useState(0)
+
+  const dolar = 5.7;
+  const euro = 5.9;
+
+
+  const Convert = (value: number,  to:string) =>{
+
+    if(to == "usd"){
+      return setShow(value * dolar)
+    }else{
+      if(to == "eur"){
+      return setShow(value * euro)
+      }
+    }
+}
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const fromConvert = fromConvertInputRef.current?.value;
+    const toConvert = toConvertInputRef.current?.value;
+    const rawValue = valueInputRef.current?.value;
+    const value = rawValue ? parseInt(rawValue, 10) : 0;
+
+
+    if ((fromConvert && fromConvert.trim() !== '')&&(toConvert && toConvert.trim() !== '')) {
+          console.log(value, toConvert)
+          Convert(value, toConvert)
+    };
+
+
+  }
+
+
   return (
     <Container>
-      <img
-        src='https://devsamurai-materials.s3.amazonaws.com/templates/avatar-gradient-dark.svg'
-        height='256'
-        alt='Dev Samurai'
+      <h1>Conversor</h1>
+      <form onSubmit={handleSubmit}>
+      <input
+        type="number"
+        placeholder="Adicione o valor"
+        ref={valueInputRef}
       />
-      <h1>Fala Samurai!</h1>
-      <p>Esse é o template typescript básico da Dev Samurai para React.</p>
+
+      <select name="toCurrency" required ref={fromConvertInputRef}>
+
+            <option value="brl">Real (BRL)</option>
+
+      </select>
+      <select  name="fromCurrency" required ref={toConvertInputRef}>
+
+            <option value="usd">Dólar Americano (USD)</option>
+            <option value="eur">Euro (EUR)</option>
+
+      </select>
+
+
+
+
+      <button type="submit">Converter</button>
+    </form >
+    <h1>{show >0 ? show: "" }</h1>
     </Container>
   )
-}
+ }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -3,4 +3,47 @@ import styled from 'styled-components'
 export const Container = styled.div`
   text-align: center;
   margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form{
+      display: flex;
+      flex-direction: column;
+      input, select {
+      font-size: 16px;
+      width: 60vh;
+      margin: 20px 0;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      outline: none;
+      transition: border-color 0.3s ease-in-out;
+      }
+
+      /* Estilização quando o campo de input está em foco */
+      input, select:focus {
+        border-color: #007bff;
+        box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+      }
+
+      button{
+        font-weight: 1000;
+        height: 60px;
+
+        border: none;
+        font-size: 30px;
+        color: white;
+        border-radius: 3px;
+        margin: 0 10px;
+        background-color: #ff0000aa;
+        margin: 20px auto;
+        cursor: pointer;
+
+        &:hover{
+          background-color: #990000;
+        }
+      }
+  }
+
 `
